a3c570e7eeec3b4447b740076bd300efa4ec5cfa
BubblingSODA <go2gym365@gmail.com>
2023-12-15 04:30:09 +0000

    divide conditional statement

--- udemy-course/src/components/Expenses/Expenses.js
+++ udemy-course/src/components/Expenses/Expenses.js
@@ -1,8 +1,10 @@
 import React, { useState } from "react";
+
 import "./Expenses.css";
-import ExpenseItem from "./ExpenseItem";
+
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
+import ExpensesList from "./ExpensesList";
 
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState("2020");
@@ -11,7 +13,7 @@ const Expenses = (props) => {
     setFilteredYear(selectedYear);
   };
 
-  const filteredExpenses = props.items.filter(expense => {
+  const filteredExpenses = props.items.filter((expense) => {
     return expense.date.getFullYear().toString() === filteredYear;
   });
 
@@ -22,14 +24,7 @@ const Expenses = (props) => {
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
-        {filteredExpenses.map((expenses) => (
-          <ExpenseItem
-            key={expenses.id}
-            title={expenses.title} 
-            amount={expenses.amount} 
-            date={expenses.date}
-        />
-        ))}        
+        <ExpensesList items={filteredExpenses}/>
       </Card>
     </div>
   );
